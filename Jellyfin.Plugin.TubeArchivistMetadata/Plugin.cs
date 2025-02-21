98472b5aa638ab2239fda2a9114d8331cc12e1f0
DarkFighterLuke <luca-consoli@live.it>
2025-02-21 17:41:51 +0100

    Fix JF->TA watched status synchronization

--- Jellyfin.Plugin.TubeArchivistMetadata/Plugin.cs
+++ Jellyfin.Plugin.TubeArchivistMetadata/Plugin.cs
@@ -173,7 +173,7 @@ namespace Jellyfin.Plugin.TubeArchivistMetadata
         private async void OnWatchedStatusChange(object? sender, UserDataSaveEventArgs eventArgs)
         {
             var user = _userManager.GetUserById(eventArgs.UserId);
-            if (user != null && Configuration.GetJFUsernamesToArray().Contains(user!.Username))
+            if (Configuration.JFTASync && user != null && Configuration.GetJFUsernamesToArray().Contains(user!.Username))
             {
                 var isPlayed = eventArgs.Item.IsPlayed(user);
                 Logger.LogDebug("User {UserId} changed watched status to {Status} for the item {ItemName}", eventArgs.UserId, isPlayed, eventArgs.Item.Name);
