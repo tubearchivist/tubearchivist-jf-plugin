7c91cafee83d6fa87824f4c1a41036751197d78a
Ivan Sop <ivan.sop93@gmail.com>
2026-09-16 21:53:54 +0200

    Added configuration where you have to specify to which libraries to apply local metadata provider.

--- Jellyfin.Plugin.TubeArchivistMetadata/Providers/EpisodeMetadataLocalProvider.cs
+++ Jellyfin.Plugin.TubeArchivistMetadata/Providers/EpisodeMetadataLocalProvider.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Jellyfin.Plugin.TubeArchivistMetadata.TubeArchivist;
 using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Library;
 using MediaBrowser.Controller.MediaEncoding;
 using MediaBrowser.Controller.Providers;
 using Microsoft.Extensions.Logging;
@@ -20,13 +23,15 @@ namespace Jellyfin.Plugin.TubeArchivistMetadata.Providers
     {
         private readonly ILogger _logger;
         private readonly IMediaEncoder _mediaEncoder;
+        private readonly ILibraryManager _libraryManager;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="EpisodeMetadataLocalProvider"/> class.
         /// </summary>
         /// <param name="mediaEncoder">Media encoder that has a path to ffprobe.</param>
+        /// <param name="libraryManager">Library manager.</param>
         /// <exception cref="DataException">If plugin not initialized, throw.</exception>
-        public EpisodeMetadataLocalProvider(IMediaEncoder mediaEncoder)
+        public EpisodeMetadataLocalProvider(IMediaEncoder mediaEncoder, ILibraryManager libraryManager)
         {
             if (Plugin.Instance == null)
             {
@@ -36,6 +41,7 @@ namespace Jellyfin.Plugin.TubeArchivistMetadata.Providers
             {
                 _logger = Plugin.Instance.Logger;
                 _mediaEncoder = mediaEncoder;
+                _libraryManager = libraryManager;
             }
         }
 
@@ -56,11 +62,35 @@ namespace Jellyfin.Plugin.TubeArchivistMetadata.Providers
             ArgumentNullException.ThrowIfNull(info);
             var path = info.Path;
 
+            var enabledLibraries = Plugin.Instance?.Configuration?.GetLocalMetadataEnabledLibrariesSet() ?? new HashSet<string>();
+
+            if (enabledLibraries.Count == 0)
+            {
+                return new MetadataResult<Episode>();
+            }
+
+            var libraries = _libraryManager.GetVirtualFolders();
+            var library = libraries.FirstOrDefault(vf =>
+                vf.Locations.Any(loc => path.StartsWith(loc, StringComparison.OrdinalIgnoreCase)));
+
+            if (library == null)
+            {
+                _logger.LogWarning("Video file not found in any library: {Path}", path);
+                return new MetadataResult<Episode>();
+            }
+
+            if (!enabledLibraries.Contains(library.Name))
+            {
+                return new MetadataResult<Episode>();
+            }
+
             if (!File.Exists(path))
             {
                 _logger.LogWarning("Video file not found: {Path}", path);
                 return new MetadataResult<Episode>();
             }
+
+            _logger.LogDebug("Getting metadata for file: {Path}", path);
 
             using var process = new Process();
             process.StartInfo = new ProcessStartInfo { FileName = _mediaEncoder.ProbePath, RedirectStandardOutput = true, UseShellExecute = false, CreateNoWindow = true };
